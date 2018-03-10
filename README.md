2646768d0e79468102541351cb9297a88b2c6a55
Gustavo H M Silva <gustavohmsilva@tuta.io>
2018-03-10 12:53:16 -0300

    Initial Commit for the Project! Looking for help!

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Lenity
-Lenity is a small project aiming to make zenity more practical in the Lua language.
+# LENITY
+Lenity is a small project aiming to make zenity more practical in the Lua language. We're aiming in an easy function oriented way to call zenity dialogs directly from Lua.
+
+This project was created mainly by my need to be able to just call zenity dialogs from inside my Lua scripts making easy to write really simple "apps" that present a visual information to users who are afraid of the terminal. In theory, with this script you are able to just type info('title','text of the info message') to open the an info box on zenity from inside of your Lua scripts for example.
+
+### Usage:
+Normally, with zenity (from inside a Lua script or the Lua interpreter):
+```
+f = io.popen(zenity --entry --title='window title' --text='window example text')
+typedByTheUser = f:read("*a"):gsub("\n","")
+f:close()
+```
+With Lenity:
+```
+typedByTheUser = entry('window title','window example text')
+```
+
+### Milestones:
+ * Fully implement tests for fields
+ * ~~Implement Entry~~
+ * ~~Implement Info~~
+ * Implement Calendar
+ * Implement Error
+ * Implement file selection
+ * Implement list
+ * Implement notification
+ * Implement progress
+ * Implement question
+ * Implement text information
+ * implement warning
+ * Implement scale
+ * ~~Implement color selection~~
+ * Implement forms
+ * Implement Password *(Maybe redundant due to entry)*
+
+### Licensing:
+This repository is entirely Open Source, licensed under GNU General Public License 3.0, 29 June 2007.   
+All work derivative work and/or third party distribution must keep the name of it's creator and it's contributors.
+
+### Contributors:
+This is the official contributors list, if you had collaborated with the project with code, please add your name to this list and someone will review the push request shortly.
+ * Gustavo Henrique Machado da Silva
---
+++ zenity.lua
@@ -0,0 +1,459 @@
+function checkTitle(title)
+	if not (title == nil) or not (title == 0) then
+		title = ' --title="'..tostring(title)..'"'
+	else
+		title = ''
+	end
+	return title
+end
+
+function checkText(text)
+	if not (text == nil) or not (title == 0) then
+		text = ' --text="'..tostring(text)..'"'
+	else
+		text = ''
+	end
+	return text
+end
+
+function checkShowPalette(showPalette)
+	assert(type(showPalette) == 'boolean')
+	if showPalette then
+		showPalette = ' --show-palette'
+	else
+		showPalette = ''
+	end
+	return showPalette
+end
+
+function checkIsPassword(isPassword)
+	assert(type(isPassword == 'boolean') or type(isPassword) == 'number')
+	if isPassword == nil or isPassword == false or isPassword == 0 then
+		isPassword = ''
+	elseif isPassword == true then
+		isPassword = " --hide-text"
+	end
+	return isPassword
+end
+
+function checkDimensions(type, x)
+	assert(type(type) == 'width' or type(type) == 'height')
+	assert(type(x) == 'number')
+	if not (x == 0) or not (x == nil) then
+		if type == 'height' then
+			x = ' --height='..x
+		elseif type == 'width' then
+			x = ' --width='..x
+		end
+	else
+		x = ''
+	end
+	return x
+end
+
+function checkTimeout(timeout)
+	if timeout == 0 or timeout == nil then
+		timeout = ''
+	else
+		assert(type(timeout) == 'number')
+		timeout = ' --timeout='..timeout
+	end
+	return timeout
+end
+
+function checkEntryText(entryText)
+	if not (entryText == 0) or not (entryText == nil) then
+		assert(type(entryText) == 'string')
+		entryText = ' --entry-text="'..entryText..'"'
+	else
+		entryText = ''
+	end
+	return entryText
+end
+
+function checkWindowIcon(windowIcon)
+	--TODO: Due to a know bug on zenity this function is not yet implemented...
+	-- Bug: https://bugs.launchpad.net/ubuntu/+source/zenity/+bug/998491
+	windowIcon = ''
+	return windowIcon
+end
+
+function checkNoWrap(noWrap)
+	assert(type(noWrap) == 'boolean' or noWrap == nil)
+	if noWrap == true then
+		noWrap = ' --no-wrap'
+	elseif noWrap == false or noWrap == nil then
+		noWrap = ''
+	end
+	return noWrap
+end
+
+function buildHex(r, g, b)
+	assert(r >= 0 and r < 256)
+	assert(g >= 0 and g < 256)
+	assert(b >= 0 and b < 256)
+	r = string.format("%x", r)
+	g = string.format("%x", g)
+	b = string.format("%x", b)
+	--[[
+	TODO: This function below is only required because zenity sucks at
+	interpreting hexadecimal colors with less than 6 chars, when zenity
+	solve this idiotic problem, this can be discarded.
+	]]--
+	r = zenitySucks(r)
+	g = zenitySucks(g)
+	b = zenitySucks(b)
+	color = ' --color=#'..r..g..b
+	return color
+end
+
+function zenitySucks(x)
+	assert(string.len(x) < 3)
+	if string.len(x) < 2 then
+		x = x..x
+	end
+	return x
+end
+
+function checkIcons(icon)
+	inList = false
+	assert(type(icon) == 'string')
+	iconsList = {
+		"address-book-new",
+		"application-exit",
+		"appointment-new",
+		"call-start",
+		"call-stop",
+		"contact-new",
+		"document-new",
+		"document-open",
+		"document-open-recent",
+		"document-page-setup",
+		"document-print",
+		"document-print-preview",
+		"document-properties",
+		"document-revert",
+		"document-save",
+		"document-save-as",
+		"document-send",
+		"edit-clear",
+		"edit-copy",
+		"edit-cut",
+		"edit-delete",
+		"edit-find",
+		"edit-find-replace",
+		"edit-paste",
+		"edit-redo",
+		"edit-select-all",
+		"edit-undo",
+		"folder-new",
+		"format-indent-less",
+		"format-indent-more",
+		"format-justify-center",
+		"format-justify-fill",
+		"format-justify-left",
+		"format-justify-right",
+		"format-text-direction-ltr",
+		"format-text-direction-rtl",
+		"format-text-bold",
+		"format-text-italic",
+		"format-text-underline",
+		"format-text-strikethrough",
+		"go-bottom",
+		"go-down",
+		"go-first",
+		"go-home",
+		"go-jump",
+		"go-last",
+		"go-next",
+		"go-previous",
+		"go-top",
+		"go-up",
+		"help-about",
+		"help-contents",
+		"help-faq",
+		"insert-image",
+		"insert-link",
+		"insert-object",
+		"insert-text",
+		"list-add",
+		"list-remove",
+		"mail-forward",
+		"mail-mark-important",
+		"mail-mark-junk",
+		"mail-mark-notjunk",
+		"mail-mark-read",
+		"mail-mark-unread",
+		"mail-message-new",
+		"mail-reply-all",
+		"mail-reply-sender",
+		"mail-send",
+		"mail-send-receive",
+		"media-eject",
+		"media-playback-pause",
+		"media-playback-start",
+		"media-playback-stop",
+		"media-record",
+		"media-seek-backward",
+		"media-seek-forward",
+		"media-skip-backward",
+		"media-skip-forward",
+		"object-flip-horizontal",
+		"object-flip-vertical",
+		"object-rotate-left",
+		"object-rotate-right",
+		"process-stop",
+		"system-lock-screen",
+		"system-log-out",
+		"system-run",
+		"system-search",
+		"system-reboot",
+		"system-shutdown",
+		"tools-check-spelling",
+		"view-fullscreen",
+		"view-refresh",
+		"view-restore",
+		"view-sort-ascending",
+		"view-sort-descending",
+		"window-close",
+		"window-new",
+		"zoom-fit-best",
+		"zoom-in",
+		"zoom-original",
+		"zoom-out",
+		"process-working",
+		"accessories-calculator",
+		"accessories-character-map",
+		"accessories-dictionary",
+		"accessories-text-editor",
+		"help-browser",
+		"multimedia-volume-control",
+		"preferences-desktop-accessibility",
+		"preferences-desktop-font",
+		"preferences-desktop-keyboard",
+		"preferences-desktop-locale",
+		"preferences-desktop-multimedia",
+		"preferences-desktop-screensaver",
+		"preferences-desktop-theme",
+		"preferences-desktop-wallpaper",
+		"system-file-manager",
+		"system-software-install",
+		"system-software-update",
+		"utilities-system-monitor",
+		"utilities-terminal",
+		"applications-accessories",
+		"applications-development",
+		"applications-engineering",
+		"applications-games",
+		"applications-graphics",
+		"applications-internet",
+		"applications-multimedia",
+		"applications-office",
+		"applications-other",
+		"applications-science",
+		"applications-system",
+		"applications-utilities",
+		"preferences-desktop",
+		"preferences-desktop-peripherals",
+		"preferences-desktop-personal",
+		"preferences-other",
+		"preferences-system",
+		"preferences-system-network",
+		"system-help",
+		"audio-card",
+		"audio-input-microphone",
+		"battery",
+		"camera-photo",
+		"camera-video",
+		"camera-web",
+		"computer",
+		"drive-harddisk",
+		"drive-optical",
+		"drive-removable-media",
+		"input-gaming",
+		"input-keyboard",
+		"input-mouse",
+		"input-tablet",
+		"media-flash",
+		"media-floppy",
+		"media-optical",
+		"media-tape",
+		"modem",
+		"multimedia-player",
+		"network-wired",
+		"network-wireless",
+		"pda",
+		"phone",
+		"printer",
+		"scanner",
+		"video-display",
+		"emblem-default",
+		"emblem-documents",
+		"emblem-downloads",
+		"emblem-favorite",
+		"emblem-important",
+		"emblem-mail",
+		"emblem-photos",
+		"emblem-readonly",
+		"emblem-shared",
+		"emblem-symbolic-link",
+		"emblem-synchronized",
+		"emblem-system",
+		"emblem-unreadable",
+		"face-angel",
+		"face-angry",
+		"face-cool",
+		"face-crying",
+		"face-devilish",
+		"face-embarrassed",
+		"face-kiss",
+		"face-laugh",
+		"face-monkey",
+		"face-plain",
+		"face-raspberry",
+		"face-sad",
+		"face-sick",
+		"face-smile",
+		"face-smile-big",
+		"face-smirk",
+		"face-surprise",
+		"face-tired",
+		"face-uncertain",
+		"face-wink",
+		"face-worried",
+		"flag-aa",
+		"application-x-executable",
+		"audio-x-generic",
+		"font-x-generic",
+		"image-x-generic",
+		"package-x-generic",
+		"text-html",
+		"text-x-generic",
+		"text-x-generic-template",
+		"text-x-script",
+		"video-x-generic",
+		"x-office-address-book",
+		"x-office-calendar",
+		"x-office-document",
+		"x-office-presentation",
+		"x-office-spreadsheet",
+		"folder",
+		"folder-remote",
+		"network-server",
+		"network-workgroup",
+		"start-here",
+		"user-bookmarks",
+		"user-desktop",
+		"user-home",
+		"user-trash",
+		"appointment-missed",
+		"appointment-soon",
+		"audio-volume-high",
+		"audio-volume-low",
+		"audio-volume-medium",
+		"audio-volume-muted",
+		"battery-caution",
+		"battery-low",
+		"dialog-error",
+		"dialog-information",
+		"dialog-password",
+		"dialog-question",
+		"dialog-warning",
+		"folder-drag-accept",
+		"folder-open",
+		"folder-visiting",
+		"image-loading",
+		"image-missing",
+		"mail-attachment",
+		"mail-unread",
+		"mail-read",
+		"mail-replied",
+		"mail-signed",
+		"mail-signed-verified",
+		"media-playlist-repeat",
+		"media-playlist-shuffle",
+		"network-error",
+		"network-idle",
+		"network-offline",
+		"network-receive",
+		"network-transmit",
+		"network-transmit-receive",
+		"printer-error",
+		"printer-printing",
+		"security-high",
+		"security-medium",
+		"security-low",
+		"software-update-available",
+		"software-update-urgent",
+		"sync-error",
+		"sync-synchronizing",
+		"task-due",
+		"task-past-due",
+		"user-available",
+		"user-away",
+		"user-idle",
+		"user-offline",
+		"user-trash-full",
+		"weather-clear",
+		"weather-clear-night",
+		"weather-few-clouds",
+		"weather-few-clouds-night",
+		"weather-fog",
+		"weather-overcast",
+		"weather-severe-alert",
+		"weather-showers",
+		"weather-showers-scattered",
+		"weather-snow",
+		"weather-storm"
+	}
+	for index, listItem in ipairs(iconsList) do
+		if icon == listItem then
+			inList = true
+		end
+	end
+	if inList then
+		icon = ' --icon-name="'..icon..'"'
+	else
+		icon = ''
+	end
+	return icon
+end
+
+function entry(title, text, entryText, isPassword, height, width, timeout)
+	-- call validation of fields
+	title = checkTitle(title)
+	text = checkText(text)
+	entryText = checkEntryText(entryText)
+	isPassword = checkIsPassword(isPassword)
+	height = checkDimensions("height", height)
+	width = checkDimensions("width", width)
+	timeout = checkTimeout(timeout)
+	-- process zenity command and return
+	local command = "zenity --entry"..title..text..entryText..isPassword..height..width..timeout
+	local f = io.popen(command)
+	local l = f:read("*a")
+	f:close()
+	return l:gsub("\n","")
+end
+
+function info(title, text, icon, noWrap, width, height)
+	title = checkTitle(title)
+	text = checkText(text)
+	icon = checkIcons(icon)
+	noWrap = checkNoWrap(noWrap)
+	width = checkDimensions('width', width)
+	height = checkDimensions('height', height)
+	command = 'zenity --info'..title..text..icon..noWrap..width..height
+	os.execute(command)
+end
+
+function colorselection(title, showPalette, r, g, b)
+	title = checkTitle(title)
+	showPalette = checkShowPalette(showPalette)
+	color = buildHex(r, g, b)
+	command = 'zenity --color-selection'..title..showPalette..color
+	local f = io.popen(command)
+	local l = f:read("*a")
+	f:close()
+	return l:gsub("\n",""):gsub("rgb%(",""):gsub("%)","")
+end
